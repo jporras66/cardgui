25ae0c01b9836a9c005f3e17f86d88b5aa647a81
Javier Porras <fjavier.porras@gmail.com>
2015-12-31 12:34:02 +0100

    some minnor changes

--- cardgui/src/main/java/com/indarsoft/cardgui/card/CardFrm.java
+++ cardgui/src/main/java/com/indarsoft/cardgui/card/CardFrm.java
@@ -84,93 +84,81 @@ public class CardFrm extends Composite {
 		groupcard.setLayout(null);
 
 		Label lblCard = new Label(groupcard, SWT.NONE);
-		lblCard.setFont(SWTResourceManager.getFont("Cantarell", 10, SWT.NORMAL));
+		lblCard.setFont(SWTResourceManager.getFont("Calibri", 9, SWT.NORMAL));
 		lblCard.setBounds(new Rectangle(0, 0, 0, 16));
 		lblCard.setBounds(12, 25, 91, 17);
 		lblCard.setText("PAN Number");
 
 		Label lblExpdate = new Label(groupcard, SWT.NONE);
-		lblExpdate.setFont(SWTResourceManager.getFont("Cantarell", 10,
-				SWT.NORMAL));
+		lblExpdate.setFont(SWTResourceManager.getFont("Calibri", 9, SWT.NORMAL));
 		lblExpdate.setBounds(new Rectangle(0, 0, 0, 16));
 		lblExpdate.setBounds(141, 25, 56, 17);
 		lblExpdate.setText("ExpDate");
 
 		Label lblService = new Label(groupcard, SWT.NONE);
-		lblService.setFont(SWTResourceManager.getFont("Cantarell", 10,
-				SWT.NORMAL));
+		lblService.setFont(SWTResourceManager.getFont("Calibri", 9, SWT.NORMAL));
 		lblService.setBounds(new Rectangle(0, 0, 0, 16));
 		lblService.setBounds(204, 25, 65, 17);
 		lblService.setText("ServCode");
 
 		txtcardpanNumber = new Text(groupcard, SWT.BORDER);
-		txtcardpanNumber.setFont(SWTResourceManager.getFont("Cantarell", 9,
-				SWT.NORMAL));
+		txtcardpanNumber.setFont(SWTResourceManager.getFont("Calibri", 9, SWT.NORMAL));
 		txtcardpanNumber.setBounds(12, 46, 135, 21);
 
 		txtcardExpDate = new Text(groupcard, SWT.BORDER);
-		txtcardExpDate.setFont(SWTResourceManager.getFont("Cantarell", 9,
-				SWT.NORMAL));
+		txtcardExpDate.setFont(SWTResourceManager.getFont("Calibri", 9, SWT.NORMAL));
 		txtcardExpDate.setBounds(152, 46, 46, 21);
 
 		txtcardServCode = new Text(groupcard, SWT.BORDER);
-		txtcardServCode.setFont(SWTResourceManager.getFont("Cantarell", 9,
-				SWT.NORMAL));
+		txtcardServCode.setFont(SWTResourceManager.getFont("Calibri", 9, SWT.NORMAL));
 		txtcardServCode.setBounds(204, 46, 46, 21);
 
 		Group pinValidationType = new Group(groupcard, SWT.SHADOW_ETCHED_IN);
 		pinValidationType.setBounds(9, 68, 244, 68);
 
 		btnVisaPvv = new Button(pinValidationType, SWT.RADIO);
-		btnVisaPvv.setFont(SWTResourceManager.getFont("Cantarell", 10,
-				SWT.NORMAL));
+		btnVisaPvv.setFont(SWTResourceManager.getFont("Calibri", 9, SWT.NORMAL));
 		btnVisaPvv.setSelection(true);
 		btnVisaPvv.setBounds(10, 15, 90, 17);
 		btnVisaPvv.setText("Visa PVV");
 
 		btnIbmOffset = new Button(pinValidationType, SWT.RADIO);
-		btnIbmOffset.setFont(SWTResourceManager.getFont("Cantarell", 10,
-				SWT.NORMAL));
+		btnIbmOffset.setFont(SWTResourceManager.getFont("Calibri", 9, SWT.NORMAL));
 		btnIbmOffset.setBounds(130, 15, 104, 17);
 		btnIbmOffset.setText("IBM Offset");
 
 		Label lblDki_2 = new Label(pinValidationType, SWT.NONE);
 		lblDki_2.setText("dki");
 		lblDki_2.setFont(SWTResourceManager
-				.getFont("Cantarell", 10, SWT.NORMAL));
+				.getFont("Calibri", 10, SWT.NORMAL));
 		lblDki_2.setBounds(new Rectangle(0, 0, 0, 16));
 		lblDki_2.setBounds(20, 40, 32, 17);
 
 		txtcarddki = new Text(pinValidationType, SWT.BORDER);
-		txtcarddki.setFont(SWTResourceManager.getFont("Cantarell", 9,
-				SWT.NORMAL));
+		txtcarddki.setFont(SWTResourceManager.getFont("Calibri", 9, SWT.NORMAL));
 		txtcarddki.setBounds(51, 38, 34, 21);
 
 		lblPvvoffset = new Label(pinValidationType, SWT.NONE);
 		lblPvvoffset.setText("pvv/offset");
-		lblPvvoffset.setFont(SWTResourceManager.getFont("Cantarell", 10,
-				SWT.NORMAL));
+		lblPvvoffset.setFont(SWTResourceManager.getFont("Calibri", 9, SWT.NORMAL));
 		lblPvvoffset.setBounds(new Rectangle(0, 0, 0, 16));
 		lblPvvoffset.setBounds(108, 38, 58, 17);
 
 		txtcardPvv = new Text(pinValidationType, SWT.BORDER);
-		txtcardPvv.setFont(SWTResourceManager.getFont("Cantarell", 9,
-				SWT.NORMAL));
+		txtcardPvv.setFont(SWTResourceManager.getFont("Calibri", 9,SWT.NORMAL));
 		txtcardPvv.setBounds(172, 38, 62, 21);
 
 		Group pinBlockFormatType = new Group(groupcard, SWT.NONE);
 		pinBlockFormatType.setBounds(9, 130, 244, 35);
 
 		btnIsopinblock0 = new Button(pinBlockFormatType, SWT.RADIO);
-		btnIsopinblock0.setFont(SWTResourceManager.getFont("Cantarell", 10,
-				SWT.NORMAL));
+		btnIsopinblock0.setFont(SWTResourceManager.getFont("Calibri", 9, SWT.NORMAL));
 		btnIsopinblock0.setSelection(true);
 		btnIsopinblock0.setText("IsoPinblock0");
 		btnIsopinblock0.setBounds(3, 15, 108, 17);
 
 		btnIsopinblock3 = new Button(pinBlockFormatType, SWT.RADIO);
-		btnIsopinblock3.setFont(SWTResourceManager.getFont("Cantarell", 10,
-				SWT.NORMAL));
+		btnIsopinblock3.setFont(SWTResourceManager.getFont("Calibri", 9, SWT.NORMAL));
 		btnIsopinblock3.setText("IsoPinblock3");
 		btnIsopinblock3.setBounds(130, 15, 108, 17);
 
@@ -190,103 +178,91 @@ public class CardFrm extends Composite {
 		groupkey.setBounds(10, 180, 470, 280);
 		groupkey.setText("Load Keys");
 		Label lblPvk = new Label(groupkey, SWT.NONE);
-		lblPvk.setFont(SWTResourceManager.getFont("Cantarell", 10, SWT.NORMAL));
+		lblPvk.setFont(SWTResourceManager.getFont("Calibri", 9, SWT.NORMAL));
 		lblPvk.setSize(23, 17);
 		lblPvk.setText("Pvk");
 		lblPvk.setLocation(20, 20);
 		lblPvk.pack();
 
 		Label lblDki = new Label(groupkey, SWT.NONE);
-		lblDki.setFont(SWTResourceManager.getFont("Cantarell", 10, SWT.NORMAL));
+		lblDki.setFont(SWTResourceManager.getFont("Calibri", 9, SWT.NORMAL));
 		lblDki.setBounds(301, 20, 25, 17);
 		lblDki.setText("dki");
 
 		Label lblPinlength = new Label(groupkey, SWT.NONE);
-		lblPinlength.setFont(SWTResourceManager.getFont("Cantarell", 10,
-				SWT.NORMAL));
+		lblPinlength.setFont(SWTResourceManager.getFont("Calibri", 9, SWT.NORMAL));
 		lblPinlength.setBounds(332, 20, 81, 17);
 		lblPinlength.setText("Pin Length");
 
 		txtkeyPvk = new Text(groupkey, SWT.BORDER);
-		txtkeyPvk.setFont(SWTResourceManager
-				.getFont("Cantarell", 9, SWT.NORMAL));
+		txtkeyPvk.setFont(SWTResourceManager.getFont("Calibri", 9, SWT.NORMAL));
 		txtkeyPvk.setEditable(false);
 		txtkeyPvk.setBounds(20, 41, 275, 21);
 
 		txtkeydki = new Text(groupkey, SWT.BORDER);
-		txtkeydki.setFont(SWTResourceManager
-				.getFont("Cantarell", 9, SWT.NORMAL));
+		txtkeydki.setFont(SWTResourceManager.getFont("Calibri", 9, SWT.NORMAL));
 		txtkeydki.setEditable(false);
 		txtkeydki.setBounds(301, 41, 36, 21);
 
 		txtkeyPinlength = new Text(groupkey, SWT.BORDER);
-		txtkeyPinlength.setFont(SWTResourceManager.getFont("Cantarell", 9,
-				SWT.NORMAL));
+		txtkeyPinlength.setFont(SWTResourceManager.getFont("Calibri", 9,SWT.NORMAL));
 		txtkeyPinlength.setEditable(false);
 		txtkeyPinlength.setBounds(350, 41, 36, 21);
 
 		Label lblNewLabel = new Label(groupkey, SWT.NONE);
-		lblNewLabel.setFont(SWTResourceManager.getFont("Cantarell", 10,
+		lblNewLabel.setFont(SWTResourceManager.getFont("Calibri", 10,
 				SWT.NORMAL));
 		lblNewLabel.setBounds(20, 80, 203, 17);
 		lblNewLabel.setText("Cvk for cvv/icvv or cvc/icvc");
 
 		Label lblDateFormat = new Label(groupkey, SWT.NONE);
-		lblDateFormat.setFont(SWTResourceManager.getFont("Cantarell", 10,
-				SWT.NORMAL));
+		lblDateFormat.setFont(SWTResourceManager.getFont("Calibri", 9, SWT.NORMAL));
 		lblDateFormat.setBounds(301, 80, 85, 17);
 		lblDateFormat.setText("Date Format");
 
 		txtkeyCvk = new Text(groupkey, SWT.BORDER);
-		txtkeyCvk.setFont(SWTResourceManager
-				.getFont("Cantarell", 9, SWT.NORMAL));
+		txtkeyCvk.setFont(SWTResourceManager.getFont("Calibri", 9, SWT.NORMAL));
 		txtkeyCvk.setEditable(false);
 		txtkeyCvk.setBounds(20, 101, 275, 21);
 
 		txtkeyDateformatCvv = new Text(groupkey, SWT.BORDER);
-		txtkeyDateformatCvv.setFont(SWTResourceManager.getFont("Cantarell", 9,
+		txtkeyDateformatCvv.setFont(SWTResourceManager.getFont("Calibri", 9,
 				SWT.NORMAL));
 		txtkeyDateformatCvv.setEditable(false);
 		txtkeyDateformatCvv.setBounds(301, 101, 76, 21);
 
 		Label lblNewLabel_1 = new Label(groupkey, SWT.NONE);
-		lblNewLabel_1.setFont(SWTResourceManager.getFont("Cantarell", 10,
-				SWT.NORMAL));
+		lblNewLabel_1.setFont(SWTResourceManager.getFont("Calibri", 9, SWT.NORMAL));
 		lblNewLabel_1.setBounds(21, 141, 167, 17);
 		lblNewLabel_1.setText("Cvk for cvv2 or cvc2");
 
 		Label label = new Label(groupkey, SWT.NONE);
-		label.setFont(SWTResourceManager.getFont("Cantarell", 10, SWT.NORMAL));
+		label.setFont(SWTResourceManager.getFont("Calibri", 9, SWT.NORMAL));
 		label.setText("Date Format");
 		label.setBounds(301, 141, 85, 17);
 
 		txtkeyCvk2 = new Text(groupkey, SWT.BORDER);
-		txtkeyCvk2.setFont(SWTResourceManager.getFont("Cantarell", 9,
-				SWT.NORMAL));
+		txtkeyCvk2.setFont(SWTResourceManager.getFont("Calibri", 9, SWT.NORMAL));
 		txtkeyCvk2.setEditable(false);
 		txtkeyCvk2.setBounds(20, 162, 275, 21);
 
 		txtkeyDateformatCvv2 = new Text(groupkey, SWT.BORDER);
-		txtkeyDateformatCvv2.setFont(SWTResourceManager.getFont("Cantarell", 9,
-				SWT.NORMAL));
+		txtkeyDateformatCvv2.setFont(SWTResourceManager.getFont("Calibri", 9, SWT.NORMAL));
 		txtkeyDateformatCvv2.setEditable(false);
 		txtkeyDateformatCvv2.setBounds(301, 162, 76, 21);
 
 		Label lblZpkForPinblock = new Label(groupkey, SWT.NONE);
-		lblZpkForPinblock.setFont(SWTResourceManager.getFont("Cantarell", 10,
-				SWT.NORMAL));
+		lblZpkForPinblock.setFont(SWTResourceManager.getFont("Calibri", 9, SWT.NORMAL));
 		lblZpkForPinblock.setBounds(20, 201, 113, 17);
 		lblZpkForPinblock.setText("Zpk for Pinblock");
 
 		txtkeyZpk = new Text(groupkey, SWT.BORDER);
-		txtkeyZpk.setFont(SWTResourceManager
-				.getFont("Cantarell", 9, SWT.NORMAL));
+		txtkeyZpk.setFont(SWTResourceManager.getFont("Calibri", 9, SWT.NORMAL));
 		txtkeyZpk.setEditable(false);
 		txtkeyZpk.setBounds(20, 222, 275, 21);
 
 		txtkeyPinblockFormat = new Text(groupkey, SWT.BORDER);
-		txtkeyPinblockFormat.setFont(SWTResourceManager.getFont("Cantarell", 9,
-				SWT.NORMAL));
+		txtkeyPinblockFormat.setFont(SWTResourceManager.getFont("Calibri", 9, SWT.NORMAL));
 		txtkeyPinblockFormat.setEditable(false);
 		txtkeyPinblockFormat.setBounds(20, 248, 275, 21);
 
@@ -302,80 +278,75 @@ public class CardFrm extends Composite {
 		result.setBounds(311, 10, 305, 163);
 
 		Label lblPin = new Label(result, SWT.NONE);
-		lblPin.setFont(SWTResourceManager.getFont("Cantarell", 10, SWT.NORMAL));
+		lblPin.setFont(SWTResourceManager.getFont("Calibri", 9, SWT.NORMAL));
 		lblPin.setBounds(new Rectangle(0, 0, 0, 16));
 		lblPin.setBounds(12, 25, 32, 17);
 		lblPin.setText("pin");
 
 		txtrspin = new Text(result, SWT.BORDER);
-		txtrspin.setFont(SWTResourceManager.getFont("Cantarell", 9, SWT.NORMAL));
+		txtrspin.setFont(SWTResourceManager.getFont("Calibri", 9, SWT.NORMAL));
 		txtrspin.setBounds(12, 46, 68, 21);
 
 		Label lblDki_1 = new Label(result, SWT.NONE);
 		lblDki_1.setFont(SWTResourceManager
-				.getFont("Cantarell", 10, SWT.NORMAL));
+				.getFont("Calibri", 10, SWT.NORMAL));
 		lblDki_1.setBounds(new Rectangle(0, 0, 0, 16));
 		lblDki_1.setBounds(87, 25, 32, 17);
 		lblDki_1.setText("dki");
 
 		txtdrsdki = new Text(result, SWT.BORDER);
 		txtdrsdki.setFont(SWTResourceManager
-				.getFont("Cantarell", 9, SWT.NORMAL));
+				.getFont("Calibri", 9, SWT.NORMAL));
 		txtdrsdki.setBounds(86, 46, 33, 21);
 
 		Label lblCvvcvc = new Label(result, SWT.NONE);
-		lblCvvcvc.setFont(SWTResourceManager.getFont("Cantarell", 10,
-				SWT.NORMAL));
+		lblCvvcvc.setFont(SWTResourceManager.getFont("Calibri", 9, SWT.NORMAL));
 		lblCvvcvc.setBounds(new Rectangle(0, 0, 0, 16));
 		lblCvvcvc.setBounds(125, 25, 32, 17);
 		lblCvvcvc.setText("cvv");
 
 		txtrscvv = new Text(result, SWT.BORDER);
-		txtrscvv.setFont(SWTResourceManager.getFont("Cantarell", 9, SWT.NORMAL));
+		txtrscvv.setFont(SWTResourceManager.getFont("Calibri", 9, SWT.NORMAL));
 		txtrscvv.setBounds(125, 46, 43, 21);
 
 		lblCvv = new Label(result, SWT.NONE);
-		lblCvv.setFont(SWTResourceManager.getFont("Cantarell", 10, SWT.NORMAL));
+		lblCvv.setFont(SWTResourceManager.getFont("Calibri", 9, SWT.NORMAL));
 		lblCvv.setBounds(new Rectangle(0, 0, 0, 16));
 		lblCvv.setBounds(174, 25, 32, 17);
 		lblCvv.setText("cvv2");
 
 		txtrscvv2 = new Text(result, SWT.BORDER);
 		txtrscvv2.setFont(SWTResourceManager
-				.getFont("Cantarell", 9, SWT.NORMAL));
+				.getFont("Calibri", 9, SWT.NORMAL));
 		txtrscvv2.setBounds(174, 46, 43, 21);
 
 		lblIcvv = new Label(result, SWT.NONE);
-		lblIcvv.setFont(SWTResourceManager.getFont("Cantarell", 10, SWT.NORMAL));
+		lblIcvv.setFont(SWTResourceManager.getFont("Calibri", 9, SWT.NORMAL));
 		lblIcvv.setBounds(new Rectangle(0, 0, 0, 16));
 		lblIcvv.setBounds(223, 25, 32, 17);
 		lblIcvv.setText("icvv\r\n");
 
 		txtrsicvv = new Text(result, SWT.BORDER);
 		txtrsicvv.setFont(SWTResourceManager
-				.getFont("Cantarell", 9, SWT.NORMAL));
+				.getFont("Calibri", 9, SWT.NORMAL));
 		txtrsicvv.setBounds(223, 46, 43, 21);
 
 		lblClearPinblock = new Label(result, SWT.NONE);
-		lblClearPinblock.setFont(SWTResourceManager.getFont("Cantarell", 10,
-				SWT.NORMAL));
+		lblClearPinblock.setFont(SWTResourceManager.getFont("Calibri", 9,SWT.NORMAL));
 		lblClearPinblock.setBounds(12, 86, 105, 17);
 		lblClearPinblock.setText("Clear Pinblock");
 
 		txtrsclearpinblock = new Text(result, SWT.BORDER);
-		txtrsclearpinblock.setFont(SWTResourceManager.getFont("Cantarell", 9,
-				SWT.NORMAL));
+		txtrsclearpinblock.setFont(SWTResourceManager.getFont("Calibri", 9,SWT.NORMAL));
 		txtrsclearpinblock.setBounds(12, 107, 135, 21);
 
 		lblEncryptedPinblock = new Label(result, SWT.NONE);
-		lblEncryptedPinblock.setFont(SWTResourceManager.getFont("Cantarell",
-				10, SWT.NORMAL));
+		lblEncryptedPinblock.setFont(SWTResourceManager.getFont("Calibri", 9, SWT.NORMAL));
 		lblEncryptedPinblock.setBounds(160, 86, 135, 17);
 		lblEncryptedPinblock.setText("Encrypted Pinblock");
 
 		txtrsencryptedpinblock = new Text(result, SWT.BORDER);
-		txtrsencryptedpinblock.setFont(SWTResourceManager.getFont("Cantarell",
-				9, SWT.NORMAL));
+		txtrsencryptedpinblock.setFont(SWTResourceManager.getFont("Calibri", 9, SWT.NORMAL));
 		txtrsencryptedpinblock.setBounds(160, 107, 135, 21);
 
 		lblrsmessage = new Label(result, SWT.NONE);
